5496b27fe6a7287d884fd563574cdbc6498e6a0f
ciscokarim <ciscokarim@gmail.com>
2023-01-03 12:40:20 +0000

    test on tester

--- 02-100 days/999_tester.py
+++ 02-100 days/999_tester.py
@@ -1,7 +1,10 @@
+""" Module providingFunction printing python version."""
 import random
-random_number=random.randint(1,5)
-malist=[1,2,3,4,5]
+random_number = random.randint(1, 5)
+malist = [1, 2, 3, 4, 5]
 
 random.shuffle(malist)
-
 print(malist)
+
+#asdfasdf
+#asdfasdfadsf
