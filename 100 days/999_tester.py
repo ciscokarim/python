d0de432f7703b3f3863ee7bdaa2f2c23da09fc4e
ciscokarim <ciscokarim@gmail.com>
2023-01-03 02:27:59 +0000

    041

--- 100 days/999_tester.py
+++ 100 days/999_tester.py
@@ -1,13 +1,7 @@
-from ssl import ALERT_DESCRIPTION_CERTIFICATE_UNOBTAINABLE
+import random
+random_number=random.randint(1,5)
+malist=[1,2,3,4,5]
 
+random.shuffle(malist)
 
-bidder_dict={"aftab":40,"basit":35,"faizan":42,"nada":39}
-
-bidder_dict={"kaka":55}
-
-print(bidder_dict)
-
-mydict={"zero":["0.0.0.0"]}     #create a dictionary with a list item in it.
-mydict={"one":["1.1.1,1"]}      #add another list item to the dictionary. IT will be appended.
-
-print(mydict)
+print(malist)
